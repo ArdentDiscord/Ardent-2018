136aebebb3e2feaa8e9022bc4db0a5e4fd5b1ce8
Adam Ratzman <adam@adamratzman.com>
2018-08-17 21:45:16 -0400

    localize /lang

--- src/main/kotlin/com/ardentbot/commands/admin/SetLanguage.kt
+++ src/main/kotlin/com/ardentbot/commands/admin/SetLanguage.kt
@@ -18,22 +18,20 @@ class SetLanguage : Command("lang", arrayOf("language", "setlang"), null) {
         val data = register.database.getGuildData(event.guild)
         val currLang = data.language ?: Language.ENGLISH
         if (arguments.isEmpty()) {
-            event.channel.send("The current language is set to []. To change this, use */lang language_here*."
-                    .apply(currLang.readable) + "\n" +
-                    "**Language list**:" + "\n" + Language.values().joinToString("\n") { "${it.readable} (${it.id})" }, register)
+            event.channel.send(translate("lang.current_info",event, register).apply(currLang.readable) + "\n" +
+                    "**${translate("lang.language_list",event, register)}**:" + "\n" + Language.values().joinToString("\n") { "${it.readable} (${it.id})" }, register)
         } else {
             val language = Language.values().find { it.id.equals(arguments.concat(), true) || it.readable.equals(arguments.concat(), true) }
-            if (language == null) event.channel.send("You specified an invalid language! Type */lang* to get a full language list.", register)
+            if (language == null) event.channel.send(translate("lang.invalid_lang",event, register), register)
             else {
                 event.channel.send(Emojis.WARNING_SIGN.cmd +
-                        "Are you sure you want to change the language from [] to []? Type `yes` if so, or `no` to cancel"
-                                .apply(currLang.readable, language.readable), register)
+                        translate("lang.warning_change",event, register).apply(currLang.readable, language.readable), register)
                 Sender.waitForMessage({ it.channel.id == event.channel.id && it.author.id == event.author.id && it.guild.id == event.guild.id }, {
                     if (it.message.contentRaw.startsWith("y", true) || it.message.contentRaw.isTranslatedPhrase("yes", event.guild, register)) {
                         data.language = language
                         register.database.update(data)
-                        event.channel.send(Emojis.HEAVY_CHECK_MARK.cmd + "Updated the server language", register)
-                    } else event.channel.send(Emojis.OK_HAND.cmd + "Canceled language update", register)
+                        event.channel.send(Emojis.HEAVY_CHECK_MARK.cmd + translate("lang.updated",event, register), register)
+                    } else event.channel.send(Emojis.OK_HAND.cmd + translate("lang.cancel_update",event, register), register)
                 })
             }
         }
