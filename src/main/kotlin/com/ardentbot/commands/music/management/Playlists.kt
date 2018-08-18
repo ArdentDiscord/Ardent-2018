f16d6a0e77c91731acca6cf1912f4f9b235755de
Adam Ratzman <adam@adamratzman.com>
2018-08-17 20:11:31 -0400

    localize: playlists

--- src/main/kotlin/com/ardentbot/commands/music/management/Playlists.kt
+++ src/main/kotlin/com/ardentbot/commands/music/management/Playlists.kt
@@ -20,137 +20,141 @@ import java.util.stream.Collectors
 @ModuleMapping("music")
 class Playlists : Command("playlist", arrayOf("playlists"), null) {
     override fun onInvoke(event: GuildMessageReceivedEvent, arguments: List<String>, flags: List<Flag>, register: ArdentRegister) {
-        when (arguments.getOrNull(0)) {
-            "play" -> {
+        val arg = arguments.getOrNull(0)
+        when {
+            arg?.isTranslatedArgument("play", event.guild, register) == true -> {
                 val playlist: DatabaseMusicPlaylist? = arguments.getOrNull(1)?.let {
                     asPojo(register.database.get("music_playlists", it) as HashMap<*, *>?, DatabaseMusicPlaylist::class.java)
                 }
-                if (playlist == null) event.channel.send("You need to specify a valid playlist id!", register)
+                if (playlist == null) event.channel.send(translate("playlists.specify_valid_id", event, register), register)
                 else {
-                    event.channel.send("Loading tracks from playlist **[]**..".apply(playlist.name), register)
+                    event.channel.send(translate("playlists.loading_tracks", event, register).apply("**${playlist.name}**"), register)
                     playlist.toLocalPlaylist(event.member).loadTracks(event.channel, event.member, register)
                 }
             }
-            "list" -> {
+            arg?.isTranslatedArgument("list", event.guild, register) == true -> {
                 val user = event.message.mentionedUsers.getOrElse(0) { event.author }
-                val embed = getEmbed("[] | Music Playlists".apply(user.display()), event.channel)
+                val embed = getEmbed(translate("playlists.list_embed_title", event, register).apply(user.display()), event.channel)
                 val playlists = register.database.getPlaylists(user.id)
-                if (playlists.isEmpty()) embed.appendDescription("This user doesn't have any playlists! Create one by typing */playlist create [name]*")
+                if (playlists.isEmpty()) embed.appendDescription(translate("playlists.no_playlists", event, register))
                 else {
                     playlists.forEachIndexed { index, playlist ->
-                        embed.appendDescription(index.diamond() + " " +
-                                "[**[]**]($base/music/playlist/[]) - ID: *[]* - Last Modified at *[]*"
-                                        .apply(playlist.name, playlist.id, playlist.id, playlist.lastModified.localeDate()) + "\n\n")
+                        embed.appendDescription(index.diamond() +
+                                "[**[]**]($base/music/playlist/[]) - " + translate("playlists.list_row", event, register)
+                                .apply(playlist.name, playlist.id, playlist.id, playlist.lastModified.localeDate()) + "\n\n")
                     }
-                    embed.appendDescription("You can play a playlist using */playlist play [playlist id]*")
+                    embed.appendDescription(translate("playlists.how_play", event, register))
                 }
                 event.channel.send(embed, register)
             }
-            "view" -> {
+            arg?.isTranslatedArgument("view", event.guild, register) == true -> {
                 val playlist: DatabaseMusicPlaylist? = arguments.getOrNull(1)?.let {
                     asPojo(register.database.get("music_playlists", it) as HashMap<*, *>?, DatabaseMusicPlaylist::class.java)
                 }
-                if (playlist == null) event.channel.send("You need to specify a valid playlist id!", register)
+                if (playlist == null) event.channel.send(translate("playlists.specify_valid_id", event, register), register)
                 else {
-                    event.channel.send("To see track information for, or modify **[]** *by []*, go to [] - id: *[]*"
+                    event.channel.send(translate("playlists.view_response", event, register)
                             .apply(playlist.name, register.getUser(playlist.owner)?.display()
-                                    ?: "Unknown", "https://ardentbot.com/music/playlist/${playlist.id}", playlist.id), register)
+                                    ?: translate("unknown", event, register), "https://ardentbot.com/music/playlist/${playlist.id}", playlist.id), register)
                 }
             }
-            "delete" -> {
+            arg?.isTranslatedArgument("delete", event.guild, register) == true -> {
                 val playlist: DatabaseMusicPlaylist? = arguments.getOrNull(1)?.let {
                     asPojo(register.database.get("music_playlists", it) as HashMap<*, *>?, DatabaseMusicPlaylist::class.java)
                 }
-                if (playlist == null) event.channel.send("You need to specify a valid playlist id!", register)
+                if (playlist == null) event.channel.send(translate("playlists.specify_valid_id", event, register), register)
                 else {
-                    if (playlist.owner != event.author.id) event.channel.send("You need to be the owner of this playlist in order to delete it!", register)
+                    if (playlist.owner != event.author.id) event.channel.send(translate("playlists.delete_permission", event, register), register)
                     else {
-                        event.channel.selectFromList(event.member, "Are you sure you want to delete the playlist []? This is irreversible"
-                                .apply(playlist.name), mutableListOf("Yes", "No"), { selection, m ->
+                        event.channel.selectFromList(event.member, translate("playlists.confirm_delete", event, register)
+                                .apply("**${playlist.name}**"), mutableListOf(translate("yes", event, register),
+                                translate("no", event, register)), { selection, m ->
                             if (selection == 0) {
                                 register.database.delete(playlist)
-                                event.channel.send(Emojis.BALLOT_BOX_WITH_CHECK.cmd + "Deleted the playlist **[]**".apply(playlist.name), register)
-                            } else event.channel.send(Emojis.BALLOT_BOX_WITH_CHECK.symbol + " " + "Cancelled playlist deletion..", register)
+                                event.channel.send(Emojis.BALLOT_BOX_WITH_CHECK.cmd +
+                                        translate("playlists.delete_success", event, register).apply("**${playlist.name}**"), register)
+                            } else event.channel.send(Emojis.BALLOT_BOX_WITH_CHECK.cmd + translate("playlists.cancel_deletion", event, register), register)
                             m.delete()
                         }, register = register)
                     }
                 }
             }
-            "create" -> {
-                if (arguments.size == 1) event.channel.send("You need to include a name for this playlist", register)
+            arg?.isTranslatedArgument("create", event.guild, register) == true -> {
+                if (arguments.size == 1) event.channel.send(translate("playlists.create_need_name", event, register), register)
                 else {
                     val name = arguments.without(0).concat()
-                    event.channel.selectFromList(event.member, "What type of playlist do you want to create?",
-                            mutableListOf("Default", "Link a Spotify Playlist or Album", "Clone someone's playlist", "Link a YouTube Playlist"), { selection, msg ->
+                    event.channel.selectFromList(event.member, translate("playlists.create_what_type", event, register),
+                            mutableListOf(translate("playlists.default", event, register), translate("playlists.link_spotify", event, register),
+                                    translate("playlists.clone_other", event, register), translate("playlists.link_youtube", event, register)), { selection, msg ->
                         msg.delete().queue()
                         when (selection) {
                             0 -> {
-                                event.channel.send("Successfully created the playlist **[]**!".apply(name), register)
+                                event.channel.send(translate("playlists.create_success", event, register).apply("**$name**"), register)
                                 val playlist = DatabaseMusicPlaylist(genId(6, "music_playlists"), event.author.id, name, System.currentTimeMillis(),
                                         null, null, null, tracks = mutableListOf())
                                 register.database.insert(playlist)
-                                event.channel.send("View this playlist online at []".apply("$base/music/playlist/${playlist.id}"), register)
+                                event.channel.send(translate("playlists.view_this_online", event, register).apply("$base/music/playlist/${playlist.id}"), register)
                             }
                             1 -> {
-                                event.channel.send("Please enter in a Spotify playlist or album url now", register)
+                                event.channel.send(translate("playlists.specify_spotify_now", event, register), register)
                                 Sender.waitForMessage({ it.author.id == event.author.id && it.channel.id == event.channel.id && it.guild.id == event.guild.id }, { reply ->
                                     val url = reply.message.contentRaw.split("?")[0]
                                     val playlist: DatabaseMusicPlaylist? = when {
                                         url.startsWith("https://open.spotify.com/album/") -> {
-                                            event.channel.send("Successfully created the playlist **[]**!".apply(name), register)
+                                            event.channel.send(translate("playlists.create_success", event, register).apply("**$name**"), register)
                                             DatabaseMusicPlaylist(genId(6, "music_playlists"), event.author.id, name, System.currentTimeMillis(),
                                                     url.removePrefix("https://open.spotify.com/album/"), null, null)
                                         }
                                         url.startsWith("https://open.spotify.com/user/") -> {
-                                            event.channel.send("Successfully created the playlist **[]**!".apply(name), register)
+                                            event.channel.send(translate("playlists.create_success", event, register).apply("**$name**"), register)
                                             DatabaseMusicPlaylist(genId(6, "music_playlists"), event.author.id, name, System.currentTimeMillis(),
                                                     null, url.removePrefix("https://open.spotify.com/user/")
                                                     .split("/playlist/").stream().collect(Collectors.joining("||")), null)
                                         }
                                         else -> {
-                                            event.channel.send("You specified an invalid url. Cancelled playlist setup.", register)
+                                            event.channel.send(translate("playlists.invalid_url_try_again", event, register), register)
                                             null
                                         }
                                     }
                                     if (playlist != null) {
                                         register.database.insert(playlist)
-                                        event.channel.send("View this playlist online at []".apply("$base/music/playlist/${playlist.id}"), register)
+                                        event.channel.send(translate("playlists.view_this_online", event, register).apply("$base/music/playlist/${playlist.id}"), register)
                                     }
                                 })
                             }
                             2 -> {
-                                event.channel.send("Please enter in an Ardent playlist id or url", register)
+                                event.channel.send(translate("playlists.specify_ardent_playlist", event, register), register)
                                 Sender.waitForMessage({ it.author.id == event.author.id && it.channel.id == event.channel.id && it.guild.id == event.guild.id }, { reply ->
                                     val url = reply.message.contentRaw.replace("https://ardentbot.com/music/playlist/", "")
                                     val playlist = getPlaylistById(url, register)
-                                    if (playlist == null) event.channel.send("You specified an invalid playlist. Please try again", register)
+                                    if (playlist == null) event.channel.send(translate("playlists.invalid_playlist_try_again", event, register), register)
                                     else {
                                         val newPlaylist = DatabaseMusicPlaylist(genId(6, "music_playlists"), event.author.id, name, System.currentTimeMillis(),
                                                 playlist.spotifyAlbumId, playlist.spotifyPlaylistId, playlist.youtubePlaylistUrl, playlist.tracks)
                                         register.database.insert(newPlaylist)
-                                        event.channel.send("Successfully cloned **[]**!".apply(playlist.name), register)
-                                        event.channel.send("View this playlist online at []".apply("$base/music/playlist/${newPlaylist.id}"), register)
+                                        event.channel.send(translate("playlists.clone_success", event, register).apply("**${playlist.name}**"), register)
+                                        event.channel.send(translate("playlists.view_this_online", event, register).apply("$base/music/playlist/${newPlaylist.id}"), register)
                                     }
                                 })
                             }
                             else -> {
-                                event.channel.send("Please specify a YouTube playlist url now.", register)
+                                event.channel.send(translate("playlists.specify_youtube_playlist", event, register), register)
                                 Sender.waitForMessage({ it.author.id == event.author.id && it.channel.id == event.channel.id && it.guild.id == event.guild.id }, { reply ->
                                     val url = reply.message.contentRaw
                                     if (url.startsWith("https://www.youtube.com/playlist?list=") || url.startsWith("https://youtube.com/playlist?list=")) {
-                                        event.channel.send("Successfully created the playlist **[]**!".apply(name), register)
+                                        event.channel.send(translate("playlists.create_success", event, register).apply("**$name**"), register)
                                         val playlist = DatabaseMusicPlaylist(genId(6, "music_playlists"), event.author.id, name, System.currentTimeMillis(),
                                                 null, null, url, tracks = mutableListOf())
                                         register.database.insert(playlist)
-                                        event.channel.send("View this playlist online at []".apply("$base/music/playlist/${playlist.id}"), register)
+                                        event.channel.send(translate("playlists.view_this_online", event, register).apply("$base/music/playlist/${playlist.id}"), register)
                                     } else {
-                                        event.channel.send("You specified an invalid url. Cancelled playlist setup.", register)
+                                        event.channel.send(translate("playlists.create_invalid_url", event, register), register)
                                     }
                                 })
                             }
                         }
                     }, failure = {
-                        event.channel.send("Cancelled playlist creation.", register)
+                        event.channel.send(translate("playlists.create_cancel", event, register), register)
                     }, register = register)
                 }
             }
